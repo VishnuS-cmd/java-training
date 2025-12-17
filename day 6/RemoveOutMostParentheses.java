04b1c3115ff22fe8e8fadf9fafd0d953c3572572
vishnu <settipallivishnju367@gmail.com>
2025-12-17 14:28:25 +0530

    i learn methods

---
+++ day 6/RemoveOutMostParentheses.java
@@ -0,0 +1,29 @@
+public class RemoveOutMostParentheses {
+    public static void main(String[] args) {
+        String s="(()()())(())";
+        System.out.println(remove(s));
+    }
+    public static String remove(String s)
+    {
+        int cnt=0;
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==')') {
+                cnt--;
+
+            }
+            if(cnt!=0)
+            {
+                sb.append(s.charAt(i));
+
+            }
+            if(s.charAt(i)=='(') {
+                cnt++;
+
+            }
+            
+        }
+        return sb.toString();
+    }
+}
