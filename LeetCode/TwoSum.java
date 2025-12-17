e47580ef022214035c2ca15ecc502416aae93a30
vishnu <settipallivishnju367@gmail.com>
2025-12-17 21:58:59 +0530

    leetcodeprogramsucess

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -1,4 +1,3 @@
-package LeetCode;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
      for(int i=0;i<nums.length;i++)
---
+++ LeetCode/reverseword.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+      StringBuilder sb = new StringBuilder();
+        String[] str = s.trim().split("\\s+");
+
+        for (int i = str.length - 1; i >= 0; i--) {
+            sb.append(str[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
